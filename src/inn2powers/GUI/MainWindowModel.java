e26210c92f463bcd61de86d4ecc77f22d42275e4
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-11-07 10:00:57 +0100

    csv

---
+++ src/inn2powers/GUI/MainWindowModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inn2powers.GUI;
+
+/**
+ *
+ * @author janvanzetten
+ */
+public class MainWindowModel {
+    
+}
