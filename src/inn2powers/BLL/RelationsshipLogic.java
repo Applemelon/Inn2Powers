7bba8727382cc0da867749b6e6073314aa9cac7a
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-11-08 15:37:44 +0100

    added Hopfully working 2. layer in the Relationsship finder

--- src/inn2powers/BLL/RelationsshipLogic.java
+++ src/inn2powers/BLL/RelationsshipLogic.java
@@ -26,30 +26,76 @@ public class RelationsshipLogic {
 
     public List<Relation> findRelationTo(Company startCompany, Company targetCompany) {
         List<Relation> relations = dm.getAllRelations();
+        List<Company> LayerOne = new ArrayList<>();
+        List<Relation> LayerOneR = new ArrayList<>();
+        List<Company> LayerTwo = new ArrayList<>();
+        List<Relation> LayerTwoR = new ArrayList<>();
 
         //first Layer check
         for (Relation relation : relations) {
-            if (checkRelation(startCompany, targetCompany, relation.getSource(), relation.getTarget())) {
-                List<Relation> list = new ArrayList<>();
-                list.add(relation);
-                return list;
+            Company check = getFreeCompanyRelation(startCompany, relation);
+            if (check != null) {
+                LayerOne.add(check);
+                LayerOneR.add(relation);
+                if (checkRelation(targetCompany, relation)) {
+                    List<Relation> list = new ArrayList<>();
+                    list.add(relation);
+                    return list;
+                }
+            }
+        }
+
+        //layer two
+        for (int i = 0; i < LayerOne.size(); i++) {
+            for (Relation relation : relations) {
+                Company check = getFreeCompanyRelation(LayerOne.get(i), relation);
+                if (check != null) {
+                    LayerTwo.add(check);
+                    LayerTwoR.add(relation);
+                    if (checkRelation(targetCompany, relation)) {
+                        List<Relation> list = new ArrayList<>();
+                        list.add(LayerOneR.get(i));
+                        list.add(relation);
+                        return list;
+                    }
+                }
             }
         }
         
         
-        
-        
-        
-        
-        
-        
-        
+        //Layer three
+        for (int i = 0; i < LayerTwoR.size(); i++) {
+            for (Relation relation : relations) {
+                Company check = getFreeCompanyRelation(LayerTwo.get(i), relation);
+                if (check != null) {
+                    LayerTwo.add(check);
+                    LayerTwoR.add(relation);
+                    if (checkRelation(targetCompany, relation)) {
+                        List<Relation> list = new ArrayList<>();
+                        list.add(LayerOneR.get(i));
+                        list.add(relation);
+                        return list;
+                    }
+                }
+            }
+            
+        }
+
         return null;
     }
 
-    private boolean checkRelation(Company start, Company target, Company start1, Company target1) {
-        return (start.equals(start1) && target.equals(target1)) || (start.equals(target1) && target.equals(start1));
+    private boolean checkRelation(Company company, Relation relation) {
+        return (company.equals(relation.getSource()) || company.equals(relation.getTarget()));
+    }
 
+    private Company getFreeCompanyRelation(Company company, Relation relation) {
+        if (company.equals(relation.getSource())) {
+            return relation.getTarget();
+        } else if (company.equals(relation.getTarget())) {
+            return relation.getSource();
+        } else {
+            return null;
+        }
     }
 
 }
