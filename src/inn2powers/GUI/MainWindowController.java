ffac45cd9af48317c8ed8a683d88472b3705db26
Applemelon <alexlolol2011@live.dk>
2017-11-09 13:54:06 +0100

    Har prøvet og sat grundlaget til at få dearch bar til at virke, men gør nok et eller andet forkert

--- src/inn2powers/GUI/MainWindowController.java
+++ src/inn2powers/GUI/MainWindowController.java
@@ -14,9 +14,11 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
@@ -160,6 +162,21 @@ public class MainWindowController implements Initializable {
                 }
             }
         }
+    }
+    
+    @FXML
+    public void searchBar() {
+        ObservableList<String> ol = FXCollections.observableArrayList();
+        FilteredList<String> filteredList;
+        filteredList = new FilteredList<>(ol);
+        listFirmaer.setItems(filteredList);
+
+        filteredList.setPredicate(new Predicate<String>() {
+            @Override
+            public boolean test(String t) {
+                return false;
+            }
+        }); 
     }
 
 }
