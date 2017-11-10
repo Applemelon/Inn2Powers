bbd3fc92a058988c69a2b6350de493968b56d545
LeaFreidahl <33053859+LeaFreidahl@users.noreply.github.com>
2017-11-10 09:59:05 +0100

    underbrancher

--- src/inn2powers/GUI/MainWindowController.java
+++ src/inn2powers/GUI/MainWindowController.java
@@ -157,13 +157,30 @@ public class MainWindowController implements Initializable {
                 List<Company> companies = BLLM.getCompaniesFromCategories(comboUnderbrancherSelected.getValue());
                 for (Company company : companies) {
                     ol.add(company.getName());
-                    accordian.getPanes().add(new TitledPane(company.getName(), new AnchorPane()));
+                    TitledPane gridTitlePane = new TitledPane(company.getName(), new AnchorPane());
+                    GridPane grid = new GridPane();
+                    grid.setVgap(4);
+                    grid.setPadding(new Insets(5, 5, 5, 5));
+                    grid.add(new Label("Country:  "), 0, 0);
+                    grid.add(new Label(company.getCountry()), 1, 0);
+                    grid.add(new Label("Address:  "), 0, 1);
+                    grid.add(new Label(company.getAddress()), 1, 1);
+                    grid.add(new Label("Website:  "), 0, 2);
+                    grid.add(new Label(company.getWebsite()), 1, 2);
+                    grid.add(new Label("Supply Chain Categories:  "), 0, 3);
+                    grid.add(new Label(company.getSupplyChainCategoriy()), 1, 3);
+                    grid.add(new Label("Business Roles:  "), 0, 4);
+                    grid.add(new Label(company.getBuisnessRole()), 1, 4);
+
+                    gridTitlePane.setContent(grid);
+
+                    accordian.getPanes().add(gridTitlePane);
                     listFirmaer.setItems(ol);
                 }
             }
-        }        
+        }
     }
-    
+
     @FXML
     public void searchBar() {
         ObservableList<String> ol = FXCollections.observableArrayList();
@@ -176,7 +193,7 @@ public class MainWindowController implements Initializable {
             public boolean test(String t) {
                 return false;
             }
-        }); 
+        });
     }
 
 }
