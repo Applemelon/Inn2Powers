2c4406a25f5a4ca47e494b5d6eeaf38ee1b5fbce
Asbjørn Mansa Jensen <asbj0378@easv265.dk>
2017-11-10 09:48:28 +0100

    Added intialization of relationDAO

--- src/inn2powers/DAL/DALManager.java
+++ src/inn2powers/DAL/DALManager.java
@@ -17,52 +17,69 @@ import java.util.List;
  *
  * @author janvanzetten
  */
-public class DALManager {
+public class DALManager
+{
+
     // Company Data Access Object
     CompanyDAO CDAO;
     // Relation Data Access Object
     RelationDAO RDAO;
 
-    public DALManager() throws IOException {
+    public DALManager() throws IOException
+    {
         this.CDAO = new CompanyDAO();
+        this.RDAO = new RelationDAO();
     }
-/**
- * return all the companies in the database
- * @return a list of companies
- */
-    public List<Company> getAllCompanies() {
+
+    /**
+     * return all the companies in the database
+     *
+     * @return a list of companies
+     */
+    public List<Company> getAllCompanies()
+    {
         return CDAO.getAllCompanies();
     }
-    
+
     /**
      * Get a company by its ID
+     *
      * @param id the id as an int
      * @return
-     * @throws Inn2PowerException 
+     * @throws Inn2PowerException
      */
-    public Company getCompany(int id) throws Inn2PowerException{
+    public Company getCompany(int id) throws Inn2PowerException
+    {
         return CDAO.getCompanyById(id);
     }
+
     /**
      * get all the buisness roles known
+     *
      * @return String array with the diffrent roles
      */
-    public String[] getBusinessRoles(){
+    public String[] getBusinessRoles()
+    {
         return CDAO.getCompanyBusinessRoles();
     }
+
     /**
      * get all the supply chain catagories (a lot)
+     *
      * @return String array withe the catagories
-     */  
-    public String[] getSupplyChainCategories (){
+     */
+    public String[] getSupplyChainCategories()
+    {
         return CDAO.getSupplyChainCategories();
     }
-    
+
     /**
-     *get all relations objects
+     * get all relations objects
+     *
      * @return a list with the objects
      */
-    public List<Relation> getAllRelations(){
+    public List<Relation> getAllRelations()
+    {
         return RDAO.getAllRelations();
     }
 }
