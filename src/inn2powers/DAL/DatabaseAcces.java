b2579fb406ade3d4d37df3b6f51a75fffa8ec111
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-11-20 13:29:37 +0100

    DAL

---
+++ src/inn2powers/DAL/DatabaseAcces.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inn2powers.DAL;
+
+import be.Company;
+
+/**
+ *
+ * @author janvanzetten
+ */
+public class DatabaseAcces {
+
+    public void addCompany(Company company) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
